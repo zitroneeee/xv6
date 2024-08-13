edb00a151b43351979ccfe7435423c8fda9d5d80
zitroneeee <362733473@qq.com>
2024-08-13 17:30:57 +0800

    代码完善

--- kernel/defs.h
+++ kernel/defs.h
@@ -171,6 +171,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+uint64          walkcowaddr(pagetable_t pagetable, uint64 va);   // lab6
 
 // plic.c
 void            plicinit(void);
@@ -182,6 +183,10 @@ void            plic_complete(int);
 void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
 void            virtio_disk_intr(void);
+
+// cow.c - lab6
+void            increfcnt(uint64 pa);
+uint8           decrefcnt(uint64 pa);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
